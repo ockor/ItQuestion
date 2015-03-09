f23706fc6cf9fc66edec727ee87ca111f4f5c527
ockor <ockor@sina.com>
2015-03-10 02:00:47 +0800

    remove cached filename cache

--- www/cache/f%%%%php%%phalcon%%www.ockor.com%%www%%app%%views%%index%%index.html.php
+++
@@ -1,6 +0,0 @@
-<div class="search" onkeydown="keydown(event)">
-	<form id="form" name="f" action="/s" class="fm">
-    <input name="question" type="text" class="input search_box" value="" placeholder="ERROR:Can't connect to MySQL server on 'localhost'">
-    <input type="button" class="btn_search" value="爱提问">
-    </form>
-</div>
